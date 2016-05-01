f37d08c7753de4e43789224fb89774c50794cb95
Sarah Bly <sbly@caliberbetaacademy.org>
2016-04-30 23:49:21 -0700

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board, position)
+    return false if [" ", "", nil].include?(board[position])
+    return true if ["X", "O"].include?(board[position])
+end
+
+
+  # code your #position_taken? method here!
